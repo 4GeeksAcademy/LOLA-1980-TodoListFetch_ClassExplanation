fa2c225e3cc8f9fa71ac7bda92794bca8a4cac56
Jose David Caro Cantor <43614556+josecaro02@users.noreply.github.com>
2024-04-13 03:03:26 +0000

    [Add Fetch class explanation for TODO List project]
    -home.jsx: Add two fecth to GET all TODOs from the API and

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	//LOGICA
+	const todosURL = "https://playground.4geeks.com/todo/"
+
+	//fetch( url de la API, metodos, body, info es un json)
+	//.then( codigo de status y el mensaje, aqui se convierte de JSON a JS)
+	//.then( manejar la información que nos llego de la API)
+	//.catch( si algo sale mal en el código de aqui es donde obtenemos la info del error)
+	// Si el metodo no se especifica en el fetch, lo interpreta como un GET
+	/*
+	fetch(getTodosURL)
+	.then((response)=>{console.log(response)
+		return response.json()
+	})
+	.then((data)=>{console.log(data)})
+	.catch((error)=>{error})
+	*/
+	const [todos, setTodos] = useState([])
+	
+	fetch(todosURL + 'users/josecaro02')
+		.then(response => response.json())
+		.then(data => {
+			setTodos(data.todos)
+		})
+		.catch()
+
+	let newTodo = {
+		"label": "lavar la ropa"
+	}
+
+	function addTodo() {
+		fetch(todosURL + 'todos/josecaro02', {
+			method: "POST",
+			body: JSON.stringify(newTodo),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(response => response.json())
+			.then(data => console.log(data))
+			.catch(error => error)
+	}
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>TODO'S</h1>
+			{todos.map((value, index) => {
+				return <h3 key={index}>{value.label}</h3>
+			})}
 		</div>
 	);
 };
